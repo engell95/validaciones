b150e5a25168bb761aad355dbfa414912dc85d3d
Engell Lopez <engelllopez59@gmail.com>
2021-01-04 13:35:31 -0600

    index de ejemplo

--- index.php
+++ index.php
@@ -1,4 +1,45 @@
 <?php
 
+    //1 - Importamos el archivo con las funciones
+    require_once('Validation/Validator.php');
+
+    //2 - Iniciamos la clase
+    $validate = new Validator();
+
+    /*
+    * 3 - Especificamos las variables y las reglas a evaluar
+    * Ejemplo de uso
+    * Nombre [Nombre de la variable recibida en POST o GET]
+    * required|min:2|max:255|pattern:text [Reglas de validación separadas por | con : para los atributos]
+    * --- Reglas Disponibles ---
+    * 1) required       [Variable requerida y no vacía]
+    * 2) min:2          [Carácter mínimo con : para definir atributo]
+    * 3) max:255        [Carácter Maximo con : para definir atributo]
+    * 4) pattern:text   [Tipo de valor : para definir atributo]
+    *   - text      - file
+    *   - url       - email
+    *   - int       - address
+    * 5) int            [Carácter tipo entero]
+    * 6) float          [Carácter tipo float]
+    */
+    $validatedData = $validate->validators([
+        'Nombre'        => 'required|min:2|max:255|pattern:text',
+        'Action'        => 'required'
+    ]);
+
+    // 3 - Validamos si existen Mensajes de error e imprimimos
+    if(!empty($validatedData)){
+        echo $validatedData;
+    }
+
+    /*
+    * Ejemplo de mensajes de error (La clase tomara el nombre de la variable como parte de los mensajes de errores)
+    * El campo Nombre requiere un minimo de 2 parametros.
+    * El campo Action es requerido
+    * URL DE ACCESO
+    * - http://localhost/validaciones/?Nombre=ss / envio de variable mediante metodo get (nombre)
+    * - Respuesta
+    * - El campo Action es requerido.
+    */
 
 ?>
